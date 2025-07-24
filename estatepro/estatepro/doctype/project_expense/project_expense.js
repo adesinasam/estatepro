6ef7c1bb61b77751e3b026a8f803f29e9cf01ded
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-07-24 08:38:38 +0100

    Enhance project expense and value addition item linkage
    Added 'expense_account' field to Value Addition Item and Value Addition Detail doctypes, with automatic fetching and filtering. Updated Project Expense form logic to clear value_addition_item on new forms, recalculate totals, and improved field dependencies and layout for better data integrity and workflow.

--- estatepro/estatepro/doctype/project_expense/project_expense.js
+++ estatepro/estatepro/doctype/project_expense/project_expense.js
@@ -2,6 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Project Expense', {
+    refresh: function(frm){
+        // Clear value_addition_item field for all rows if form is new
+        if (frm.is_new()) {
+            (frm.doc.expenses || []).forEach(function(row) {
+                frappe.model.set_value(row.doctype, row.name, 'value_addition_item', '');
+            });
+        }
+    },
+
     setup: function(frm) {
         frm.set_query('paying_account', function() {
             return {
@@ -11,5 +20,50 @@ frappe.ui.form.on('Project Expense', {
                 }
             };
         });
+    },
+
+    amount: function(frm) {
+        // Recalculate when amount changes
+        calculate_totals(frm);
+    },
+
+    supplier: function(frm) {
+        // Fetch and set sales_team when customer changes
+        if (!frm.doc.supplier) {
+            frm.set_value("payment_to", "");
+            return;
+        }
+        if (frm.doc.supplier) {
+            frm.set_value("payment_to", frm.doc.supplier);
+            return;
+        }
     }
 });
+
+frappe.ui.form.on('Value Addition Detail', {
+    expenses_remove: function(frm, cdt, cdn) {
+        calculate_totals(frm);
+    },
+
+    amount_spent: function(frm, cdt, cdn) {
+        calculate_totals(frm);
+    },
+
+    value_addition_item: function(frm, cdt, cdn) {
+        calculate_totals(frm);
+    }
+});
+
+function calculate_totals(frm) {
+    let total_expense = 0;
+    
+    if (frm.doc.expenses) {
+        frm.doc.expenses.forEach(function(row) {
+            total_expense += flt(row.amount_spent || 0);
+            difference_amount = flt(total_expense || 0) - flt(frm.doc.amount || 0);
+        });
+    }
+    
+    frm.set_value("total_expense", total_expense);
+    frm.set_value("difference_amount", difference_amount);
+}
