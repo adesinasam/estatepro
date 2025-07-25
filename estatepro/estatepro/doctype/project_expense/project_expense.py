c9796abb928f3fd66720bf020f8d19e644c6aa36
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-07-25 16:57:39 +0100

    Update project_expense.py

--- estatepro/estatepro/doctype/project_expense/project_expense.py
+++ estatepro/estatepro/doctype/project_expense/project_expense.py
@@ -187,34 +187,34 @@ class ProjectExpense(Document):
                     if not expense_account:
                         frappe.throw(f"No Expense Account found for Value Addition Item {expense_row.value_addition_item}")
 
-                 #Create a Journal Entry
-                 journal_entry = frappe.new_doc("Journal Entry")
-                 journal_entry.voucher_type = "Journal Entry"
-                 je.company = self.company
-                 journal_entry.posting_date = self.expense_date
-                 journal_entry.user_remark = f"Valuation addition expense for Estate Project {self.estate_project}"
+                #Create a Journal Entry
+                journal_entry = frappe.new_doc("Journal Entry")
+                journal_entry.voucher_type = "Journal Entry"
+                je.company = self.company
+                journal_entry.posting_date = self.expense_date
+                journal_entry.user_remark = f"Valuation addition expense for Estate Project {self.estate_project}"
 
-                 # 1 Create Debit entry (Land Stock)
-                 journal_entry.append("accounts", {
-                     "account": land_inventory_accont,
-                     "debit_in_account_currency": self.total_expense,
-                     "credit_in_account_currency": 0,
-                     "project": self.project,
-                     "cost_center": self.cost_center
-                 })
+                # 1 Create Debit entry (Land Stock)
+                journal_entry.append("accounts", {
+                    "account": land_inventory_accont,
+                    "debit_in_account_currency": self.total_expense,
+                    "credit_in_account_currency": 0,
+                    "project": self.project,
+                    "cost_center": self.cost_center
+                })
 
-                 # 2 Create Credit entry (Paying Account)
-                 journal_entry.append("accounts", {
-                     "account": paying_account,
-                     "credit_in_account_currency": self.total_expense,
-                     "debit_in_account_currency": 0,
-                     "project": self.project,
-                     "cost_center": self.cost_center
-                 })
+                # 2 Create Credit entry (Paying Account)
+                journal_entry.append("accounts", {
+                    "account": paying_account,
+                    "credit_in_account_currency": self.total_expense,
+                    "debit_in_account_currency": 0,
+                    "project": self.project,
+                    "cost_center": self.cost_center
+                })
 
-                    # Submit the Journal Entry
-                    journal_entry.insert(ignore_permissions=True)
-                    journal_entry.submit()
+                # Submit the Journal Entry
+                journal_entry.insert(ignore_permissions=True)
+                journal_entry.submit()
 
             else:
                 # Create a Journal Entry
